7ad0a252bc17b4815f95080b1090957ed309b4a9
NikharPanchal <panchalnikhar4920@gmail.com>
2023-02-02 18:11:09 +0530

    String and array program

---
+++ Javascript/String.js
@@ -0,0 +1,17 @@
+const str = "This is a Javascript";
+
+console.log(str.length);
+console.log(str.indexOf('i'));
+console.log(str.lastIndexOf('i'));
+console.log(str.slice(10));
+console.log(str.slice(10, 14));
+
+const arr = [1, 67, 3, 2, 5, 6, 8];
+console.log(arr.splice(-1));
+console.log(arr.splice(1, 2));
+console.log(arr);
+
+console.log(arr.reverse());
+
+console.log(arr.join(''));
+
