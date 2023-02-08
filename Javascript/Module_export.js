238000252efc015ac500b7f47b3955b2fd737f4e
NikharPanchal <panchalnikhar4920@gmail.com>
2023-02-08 18:21:08 +0530

    javascript model changes in bootstrap template

---
+++ Javascript/Module_export.js
@@ -0,0 +1,4 @@
+const email = 'panchlnikhar4920@gmail.com';
+const password = 'nikhar123';
+
+export { email, password }
---
+++ Javascript/Module_import.js
@@ -0,0 +1,4 @@
+import { email, password } from "./Module_export.js";
+
+console.log(email);
+console.log(password);
